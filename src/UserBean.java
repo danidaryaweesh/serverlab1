6b65a73ddab3716c3c4116700b6d1afe40743ff9
danidaryaweesh <dani_kurkees@hotmail.com>
2016-11-18 14:14:37 +0100

    new start!

---
+++ src/UserBean.java
@@ -0,0 +1,29 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Created by dani on 2016-11-18.
+ */
+@ManagedBean(name="beanTest", eager = true)
+@SessionScoped
+public class UserBean {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
